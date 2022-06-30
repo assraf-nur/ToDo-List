8057e7cd18c90f367c2d16a511cb44ba28fc77e0
Assraf Nur <92791009+assraf-nur@users.noreply.github.com>
2022-06-30 21:13:46 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Home/Home';
+import Completed from './Components/Home/Completed';
+import Calender from './Components/Home/Calender';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mx-auto'>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='home' element={<Home></Home>}></Route>
+        <Route path='completed' element={<Completed></Completed>}></Route>
+        <Route path='calender' element={<Calender></Calender>}></Route>
+      </Routes>
     </div>
   );
 }
