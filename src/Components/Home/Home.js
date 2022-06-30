03fe52c75ff4142517dbc9a221eb6b62735df9a0
Assraf Nur <92791009+assraf-nur@users.noreply.github.com>
2022-06-30 21:25:12 +0600

    add form

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h2>Home</h2>
+            <h2 className='text-center text-5xl font-semibold mt-12'>ToDo List</h2>
+            <form className='text-center mt-12'>
+              <textarea class="textarea textarea-bordered" placeholder="Bio"></textarea>
+              <br/>
+              <input className='btn btn-sm mt-5' type="submit" name="submit" value="Add"/>
+            </form>
         </div>
     );
 };
