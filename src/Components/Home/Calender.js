3ddf29f10e1e12036ffe183bc8e5ae153b253dad
Assraf Nur <92791009+assraf-nur@users.noreply.github.com>
2022-07-01 14:58:19 +0600

    add calender

--- src/Components/Home/Calender.js
+++ src/Components/Home/Calender.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 const Calender = () => {
+    const [value, onChange] = useState(new Date());
     return (
         <div>
-            <h2>Calender</h2>
+            <h2 className='text-center text-5xl font-semibold'>Calender</h2>
+            <div className='mx-auto mt-12 text-center'>
+                <Calendar onChange={onChange} value={value} className='mx-auto'></Calendar>
+            </div>
         </div>
     );
 };
